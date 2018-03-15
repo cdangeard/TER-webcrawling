b0ddb991e48640431713760f2f54b83d06179148
Drimer <ceddang@yahoo.fr>
2018-03-15 14:40:25 +0100

    petite réorganisation

--- code/scraping/cvsExtract.py
+++ code/scraping/cvsExtract.py
@@ -7,32 +7,9 @@ import csv
 from tableExtract import raceExtract,raceinfoExtract, driversExtract, allraceExtract, practiceExtract
 from linkExtract import yearUrlExtract, raceUrlExtract, practiceUrl
 
-url = 'https://www.formula1.com/en/results.html'
-liste_dates = yearUrlExtract(url)
-print(liste_dates)
-
 def sortiecvs(table, url):
     filename = url.replace(".html","").replace("https://www.formula1.com/","").replace("/","-") + ".csv"
     out = csv.writer(open('cvs/' + filename,"w"), delimiter=',',quoting=csv.QUOTE_ALL)
     for colonne in table:
         out.writerow(colonne)
     return None
-
-for annee in liste_dates[1:]:
-    print('\n\n ########'+annee+'######## \n\n')
-    print('\n #####race###### \n')
-    print(allraceExtract(annee))
-    driver = annee.replace('races','drivers')
-    print('\n #####'+driver+'###### \n')
-    print(driversExtract(driver))
-    liste_courses = raceUrlExtract(annee)
-    print(liste_courses)
-    for course in liste_courses[1:]:
-        print('\n #####'+course+'#######"" \n')
-        print(raceinfoExtract(course))
-        print('\n')
-        print(raceExtract(course))
-        practices = practiceUrl(course)
-        print('\n')
-        for practice in practices:
-            print(practiceExtract(practice))
--- code/scraping/main.py
+++ code/scraping/main.py
@@ -6,7 +6,28 @@ from urllib.request import urlopen
 import csv
 from tableExtract import raceExtract,raceinfoExtract, driversExtract, allraceExtract, practiceExtract
 from linkExtract import yearUrlExtract, raceUrlExtract, practiceUrl
+from cvsExtract import sortiecvs
 
-#print(practiceUrl('https://www.formula1.com/en/results.html/1959/races/170/indianapolis-500.html'))
-#print(raceExtract('https://www.formula1.com/en/results.html/1959/races/170/indianapolis-500.html'))
-print(practiceUrl('https://www.formula1.com/en/results.html/1996/races/638/australia/practice-2.html'))
+
+url = 'https://www.formula1.com/en/results.html'
+liste_dates = yearUrlExtract(url)
+print(liste_dates)
+
+for annee in liste_dates[1:]:
+    print('\n\n ########'+annee+'######## \n\n')
+    print('\n #####race###### \n')
+    print(allraceExtract(annee))
+    driver = annee.replace('races','drivers')
+    print('\n #####'+driver+'###### \n')
+    print(driversExtract(driver))
+    liste_courses = raceUrlExtract(annee)
+    print(liste_courses)
+    for course in liste_courses[1:]:
+        print('\n #####'+course+'#######"" \n')
+        print(raceinfoExtract(course))
+        print('\n')
+        print(raceExtract(course))
+        practices = practiceUrl(course)
+        print('\n')
+        for practice in practices:
+            print(practiceExtract(practice))
