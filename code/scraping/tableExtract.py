ad9cc0353d8bec1913ab2c7dcfc04eae95ee35ff
cdangeard <ceddang@yahoo.fr>
2018-04-05 10:27:50 +0200

    com

--- code/scraping/tableExtract.py
+++ code/scraping/tableExtract.py
@@ -5,9 +5,8 @@ from urllib.request import urlopen
 #librairie pour l'export en csv
 import csv
 
-
-
 #retourne le tableau sous forme d'un array si csv est faux, sinon ne retourne rien mais export le tableau au format csv
+#retourne les infos de la page race + all:  dates, grand prix, prenoms et nom du vainceur , equipes nombre de tours, et temps
 def allraceExtract(url, csvreturn = False):
     #ouverture d'un client url
     uClient = urlopen(url)
@@ -37,6 +36,7 @@ def allraceExtract(url, csvreturn = False):
         time.append(ligne.find("td", "dark bold hide-for-tablet").text)
     return tableau
 
+#retourne les infos sur un course (titre, date, et nom du circuit)
 def raceinfoExtract(url):
     uClient = urlopen(url)
     #stockage du code html de la page
@@ -49,6 +49,7 @@ def raceinfoExtract(url):
     circuit = page_soup.find("p", "date").find("span","circuit-info").text
     return [titre, date, circuit]
 
+#retourne les positions de départ et numero du coureur
 def gridExtract(url):
 #exrait la position en début du course et le numero des joueurs
     uClient = urlopen(url)
@@ -81,7 +82,7 @@ def gridExtract(url):
             return tableau
     return []
 
-
+# retourne, position, numero, nom, prenom, et equipe des coureurs (page races + pays)
 def raceExtract(url):
     #ouverture d'un client url
     uClient = urlopen(url)
@@ -130,6 +131,7 @@ def driversExtract(url):
         car.append(ligne.find("a","grey").text)
     return tableau
 
+#retourne les infos, des differentes pages practices, prenom et nom des coureurs
 def practiceExtract(url):
     #ouverture d'un client url
     uClient = urlopen(url)
